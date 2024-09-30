48e335f98a8676bcad21ff813ca194f5b3c797a2
Bernardo França Coelho <112012405+bernardo1310@users.noreply.github.com>
2024-09-30 08:06:41 -0300

    Atualizar o Ex10.java

--- Ex10.java
+++ Ex10.java
@@ -1,9 +1,35 @@
-
+import java.util.Scanner;
 public class Ex10 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+        Scanner read =new Scanner(System.in);
+        int[] dt = new int[3]; 
+        while(true){
+            System.out.print("digite a data");
+            String ent = read.nextLine();
+            String[] partes = read.split("/");
+            if(partes.length != 3){
+                System.out.print("data invalida");
+                continue;
+            } 
+            boolean datavalid = true;
+            for(int i=0;i<partes.length; i++){
+                if(!partes[i].matches("\\d+")){
+                    datavalid = false;
+                    break;
+                }
+                dt[i] = Integer.parseInt(partes[i]);
+            }
+            if(datavalid) {
+                int dia = dt[0];
+                int mes = dt[1];
+                int ano = dr[2];
+                if(mes<1 || mes>12){
+                    datavalid=false;
+                } else {
+                    int[] dianomes = {0,31,28 +(ano%4==0 && (ano%100!=0 || ano%400 == 0) ? 1 : 0),31,30,31,30,31,31,30,31,30,31};
+                    if(dia<1 || dia>dianomes[mes]
+                }
+            }
+         }
 	}
-
 }
